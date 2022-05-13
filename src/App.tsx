ad8f6262cdc93859742152fc620e0ab538ea895b
josie.zhou <juliett.zuluh@gmail.com>
2022-05-13 11:30:39 +0800

    refector: update code to lexical playground version

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,26 @@
 import LexicalComposer from '@lexical/react/LexicalComposer';
 import { $getRoot, $getSelection } from 'lexical';
-import LexicalContentEditable from '@lexical/react/LexicalContentEditable';
-import LexicalPlainTextPlugin from '@lexical/react/LexicalPlainTextPlugin';
-import LexicalOnChangePlugin from '@lexical/react/LexicalOnChangePlugin';
+import ContentEditable from '@lexical/react/LexicalContentEditable';
+import RichTextPlugin from '@lexical/react/LexicalRichTextPlugin';
+import { createWebsocketProvider } from './collaboration';
 import { CollaborationPlugin } from "@lexical/react/LexicalCollaborationPlugin";
-import * as Y from 'yjs';
-import { WebsocketProvider } from 'y-websocket';
 import './App.css'
 
 function App() {
-  function onChange(editorState) {
-    editorState.read(() => {
-      // Read the contents of the EditorState here.
-      const root = $getRoot();
-      const selection = $getSelection();
-
-      console.log(root, selection);
-    });
-  }
   const initialConfig = {
     onError: () => { }
   };
 
   return (
     <LexicalComposer initialConfig={initialConfig}>
-      <LexicalPlainTextPlugin
-        contentEditable={<LexicalContentEditable className="editor-input" />}
-        placeholder={<div >hello</div>}
+      <RichTextPlugin
+        contentEditable={<ContentEditable className="editor-input"/>}
+        placeholder={<div>type here</div>}
+        initialEditorState={null}
       />
-      <LexicalOnChangePlugin onChange={onChange} />
       <CollaborationPlugin
-        id="yjs-plugin"
-        providerFactory={(id, yjsDocMap) => {
-
-          const doc = new Y.Doc();
-          yjsDocMap.set(id, doc);
-
-          const provider = new WebsocketProvider(
-            "ws://localhost:1234",
-            id,
-            doc
-          );
-          console.log(provider)
-          return provider;
-        }}
+        id="main"
+        providerFactory={createWebsocketProvider}
         shouldBootstrap={true}
       />
     </LexicalComposer>
